8481b3bff179f2134980db282b4f66b96a50134f
psyomn <lethaljellybean@gmail.com>
2011-10-14 22:50:43 -0400

    Mockup for adding a project gui

---
+++ GUIAddProject.rb
@@ -0,0 +1,46 @@
+#author::Simon Symeonidis 
+# This is the GUI for managing tasks 
+
+require 'Qt4'
+
+class GUIAddProject < Qt::Widget 
+  attr_reader :ID 
+  attr_reader :WindowTitle
+public 
+  def initialize(parent=nil)
+    super(parent)
+
+    @WindowTitle = "Add Project!"
+
+    currentprojects_label = Qt::Label.new("Current Projects")
+    numofprojects_label = Qt::Label.new("Number of Projects: " + 4.to_s) 
+    addproject_button = Qt::PushButton.new("Add")
+    removeproject_button = Qt::PushButton.new("Remove")
+    cancel_button = Qt::PushButton.new("Cancel")
+    project_list = Qt::ListWidget.new()
+
+    #layouts 
+    vbox = Qt::VBoxLayout.new() 
+    grid = Qt::GridLayout.new() 
+
+    grid.addWidget(numofprojects_label,0,0) 
+    grid.addWidget(cancel_button,0,1) 
+    grid.addWidget(removeproject_button,0,2) 
+    grid.addWidget(addproject_button,0,3) 
+
+    vbox.addWidget(currentprojects_label) 
+    vbox.addWidget(project_list) 
+    vbox.addLayout(grid) 
+
+    setWindowTitle(@WindowTitle)
+    setLayout(vbox) 
+
+  end 
+private 
+end 
+
+app = Qt::Application.new(ARGV) 
+gui = GUIAddProject.new() 
+gui.show()
+app.exec()
+
