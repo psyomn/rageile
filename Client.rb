8f3a0f2ba88a74747ab12769eec1fd6e736a9e4f
psyomn <lethaljellybean@gmail.com>
2011-12-08 23:31:17 -0500

    Minor commits; Client and server are stubs.

---
+++ Client.rb
@@ -0,0 +1,14 @@
+# Generic client to communicate with a server instance
+# holding task information 
+#Author::Simon Symeonidis 
+
+class Client 
+
+  attr_reader :SocketHandle 
+  attr_accessor :SendBuffer
+
+  def initialize
+    
+  end 
+
+end 
---
+++ Server.rb
@@ -0,0 +1,6 @@
+# Generic server for the main program to listen from clients
+# and interact with 
+#Author::Simon Symeonidis 
+
+class Server 
+end 
