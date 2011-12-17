243daaf88e32d3aa82b75abcac8e7d584262c02d
psyomn <lethaljellybean@gmail.com>
2011-12-17 09:40:06 -0500

    Adding generic DBHandle! The generic DBHandle. This will be able to use alternative forms of storage such as sqlite3, mysql etc.

---
+++ gateways/DBHandle.rb
@@ -0,0 +1,28 @@
+require 'singleton' 
+
+# This acts like a db registry. Singleton and allows the gateways
+# to perform any needed db query. 
+#Author::Simon Symeonidis 
+def DBHandle 
+  include Singleton 
+  attr_accessor :Handle
+
+  # Upon initializement, create the connection to the database. 
+  # The datbase name is retrieved from the .conf text file. 
+  # Therefore notice how the Configuration class is used. 
+  def initialize 
+    @Handle = SQLite3::Database.new(Configuration.instance.Attributes["DATABASE_NAME"]) 
+  end 
+
+  # executes sql query, no return values 
+  def executeUpdate(sql) 
+    @Handle.execute(sql)
+  end 
+
+  # executes a sql query and has return values
+  # This method should accept prepared statements
+  def execute(sql) 
+    return @Handle.execute(sql)
+  end 
+
+end 
