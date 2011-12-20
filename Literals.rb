f2eef7a5c08a14ffae51583618254f840185809d
psyomn <lethaljellybean@gmail.com>
2011-12-20 11:28:32 -0500

    Documentation fix.

--- Literals.rb
+++ Literals.rb
@@ -1,6 +1,8 @@
 # This just stores literals which saves us
 # from hardcoding them in many files. 
 module Literals
+  # Configuration file tag for sqlite type database
   CONF_SQLITE_TAG = "SQLITE"
+  # Configuration file tag for mysql type database
   CONF_MYSQL_TAG = "MYSQL"
 end 
